873e4bed774c6d038066f32fd72c433d9192597d
anirudhk27 <151894664+anirudhk27@users.noreply.github.com>
2024-04-16 20:28:46 +0530

    Add files via upload

---
+++ card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Card(props){
+    return <div>
+            <h2>{props.name}</h2>
+            <img src={props.src} alt={props.alt}></img>
+            <p>{props.tel}</p>
+            <p>{props.email}</p>
+           </div>
+}
+
+export default Card
